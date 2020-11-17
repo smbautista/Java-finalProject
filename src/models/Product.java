30edb9176d728e06b613ff7cb38f37471cf69ddf
LoremZyn14 <yasuaimo@gmail.com>
2020-11-18 07:51:34 +0800

    add pages

--- src/models/Product.java
+++ src/models/Product.java
@@ -11,4 +11,95 @@ package models;
  */
 public class Product {
     
+    private int id;
+    private String name;
+    private String brand;
+    private double price;
+    private int stocks;
+    private String description;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the brand
+     */
+    public String getBrand() {
+        return brand;
+    }
+
+    /**
+     * @param brand the brand to set
+     */
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    /**
+     * @return the price
+     */
+    public double getPrice() {
+        return price;
+    }
+
+    /**
+     * @param price the price to set
+     */
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    /**
+     * @return the stocks
+     */
+    public int getStocks() {
+        return stocks;
+    }
+
+    /**
+     * @param stocks the stocks to set
+     */
+    public void setStocks(int stocks) {
+        this.stocks = stocks;
+    }
+
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
 }
